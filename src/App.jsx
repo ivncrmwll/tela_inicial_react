6f6843e03cef02f552b4f733ef6cb439b915d573
Ivan Cromwell <ivn.crmwll@gmail.com>
2026-03-10 19:52:34 -0400

    Criando a tela inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Footer from './components/footer'
+
+function App() {
+return (
+<div>
+<h1>My Daily Habits</h1>
+<p>Gerencie seus hábitos diários de forma simples e visual.</p>
+<Footer />
+</div>
+)
+}
+export default App
